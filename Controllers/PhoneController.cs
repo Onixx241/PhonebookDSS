b91f65c697e38ff08a6d24583074c653c898b7b6
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-08-01 20:06:39 -0400

    initial upload

---
+++ Controllers/PhoneController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhonebookwithAuth.Controllers
+{
+    public class PhoneController : Controller
+    {
+        public ActionResult AddContact()
+        {
+            //this is going to call addcontact stored procedure
+            return new EmptyResult();
+        }
+        public ActionResult DeleteContact()
+        {
+            //this is going to call deletecontact stored procedure
+            return new EmptyResult();
+        }
+        [Authorize]
+        public ActionResult PhonebookView()
+        {
+
+            ViewBag.Userr = User.Identity.Name;
+            string[] arr = { "hello", "Hello1", "Hello1", "Hello1", "Hello1", "Hello1", "Hello1" };
+            ViewBag.TestContacts = arr;
+            ViewBag.Message = "Test";
+            //temp return value
+            return View("PhonebookView");
+        }
+    }
+}
---
+++ Models/Phonebook/SqlHandler.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+public class SqlModel
+{
+    public string dataSource { get; set; }
+    public string initialCatalog { get; set; }
+    public string userID { get; set; }
+    public string password { get; set; }
+
+    public string sampleString { get; set; }
+
+    public SqlConnectionStringBuilder BuildConnectionString()
+    {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+        {
+            DataSource = this.dataSource,
+            InitialCatalog = this.initialCatalog,
+            UserID = this.userID,
+            Password = this.password
+        };
+
+        return builder;
+    }
+    //DataSource=;InitialCatalog=;UserID=;Password=;
+
+    //example to run stored procedure from sqlConn
+    public void runSqlQuery()
+    {
+        var builder = BuildConnectionString();
+
+        string connectionString = builder.ConnectionString;
+
+        //string command = "SELECT * FROM dbo.test_table";
+
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+
+            conn.Open();
+
+            using (SqlCommand cmd = new SqlCommand("dbo.USP_GET_REPORT_FREQUENT_LANDLORD", conn))
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@REFERRAL_DATE_RANGE", 120);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    this.sampleString = $"SQL result: {reader[0].ToString()}, {reader[1].ToString()}, {reader[2].ToString()}, {reader[3].ToString()}, {reader[4].ToString()}";
+                    reader.NextResult();
+                }
+
+            }
+        }
+    }
+
+}
