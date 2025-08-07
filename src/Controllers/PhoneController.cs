63d8e365f1250c34b87d030bf4d988c3bdfe34c8
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-08-07 10:07:26 -0400

    Cleaned up PhoneController

--- src/Controllers/PhoneController.cs
+++ src/Controllers/PhoneController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,27 +8,19 @@ namespace PhonebookwithAuth.Controllers
 {
     public class PhoneController : Controller
     {
-        public ActionResult AddContact()
-        {
-            //this is going to call addcontact stored procedure
-            
-            return new EmptyResult();
-        }
-        public ActionResult DeleteContact()
-        {
-            //this is going to call deletecontact stored procedure
-            return new EmptyResult();
-        }
+
+
         [Authorize]
         [HttpGet]
         public ActionResult Create() 
         {
             return View("Create");
         }
-        [HttpPost]
-        public ActionResult Create(string F, string L, string A, string C, string S, string Z) 
-        {
 
+        [HttpPost]
+        public ActionResult Createe(string F, string L, string A, string C, string S, string Z) 
+        {
+            //why is this not running
             Contact contact = new Contact()
             {
                 firstName = F,
@@ -39,8 +31,12 @@ namespace PhonebookwithAuth.Controllers
                 zip = Convert.ToInt32(Z)
             };
 
+            SqlModel sql = new SqlModel("D2E2SQLDEV16\\SQL19DEVF", "FNETINT01", "fnetuser", "Fnetdev@2016", User.Identity.Name);
 
-            return View("Create");
+            sql.CreateContact(contact, User.Identity.Name);
+            System.Diagnostics.Debug.WriteLine(" This is working!");
+            return RedirectToAction("PhonebookView");
+
         }
 
 
@@ -53,7 +49,7 @@ namespace PhonebookwithAuth.Controllers
 
             SqlModel model = new SqlModel("D2E2SQLDEV16\\SQL19DEVF","FNETINT01","fnetuser", "Fnetdev@2016", User.Identity.Name);
 
-            //temp return value
+            
             return View("PhonebookView", model);
         }
 
