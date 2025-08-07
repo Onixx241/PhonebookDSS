516065e1bf07ae4460ec5726d8993a0b3715a87e
Onixx241 <102994107+Onixx241@users.noreply.github.com>
2025-08-07 11:16:30 -0400

    cleaning up

--- src/Controllers/PhoneController.cs
+++ src/Controllers/PhoneController.cs
@@ -17,6 +17,7 @@ namespace PhonebookwithAuth.Controllers
             return View("Create");
         }
 
+        [Authorize]
         [HttpPost]
         public ActionResult Createe(string F, string L, string A, string C, string S, string Z) 
         {
@@ -39,39 +40,27 @@ namespace PhonebookwithAuth.Controllers
 
         }
 
+        //continue here
+        public ActionResult DeleteContact(int ID)
+        {
 
+            return RedirectToAction("PhonebookView");
+        }
+
+
+
+        //add alternate details view to view actual numbers
         [Authorize]
         public ActionResult PhonebookView()
         {
 
             ViewBag.Userr = User.Identity.Name;
-            ViewBag.bag = User.Identity.AuthenticationType;
 
             SqlModel model = new SqlModel("D2E2SQLDEV16\\SQL19DEVF","FNETINT01","fnetuser", "Fnetdev@2016", User.Identity.Name);
 
-            
+            //temp return value
             return View("PhonebookView", model);
         }
-
-
-        public ActionResult CreateContact(string F, string L, string A, string C, string S, string Z) 
-        {
-            Contact contact = new Contact()
-            {
-                firstName = F,
-                lastName = L,
-                address = A,
-                city = C,
-                State = S,
-                zip = Convert.ToInt32(Z)
-            };
-
-            SqlModel sql = new SqlModel("D2E2SQLDEV16\\SQL19DEVF", "FNETINT01", "fnetuser", "Fnetdev@2016", User.Identity.Name);
-
-            sql.CreateContact(contact, User.Identity.Name);
-            System.Diagnostics.Debug.WriteLine(" This is working!");
-            return RedirectToAction("PhonebookView");
-
-        }
+        
     }
 }
